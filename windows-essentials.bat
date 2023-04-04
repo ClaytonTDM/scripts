5a292d900d2ab1779ca5cc19d92e31082a851ecc
ClaytonTDM <claytontdm@gmail.com>
2023-04-03 21:32:35 -0400

    Update windows-essentials.bat

--- windows-essentials.bat
+++ windows-essentials.bat
@@ -1,20 +1,20 @@
 rem # --- Discord --- #
-winget install discord.discord --accept-package-agreements --accept--source-agreements
+winget install discord.discord --accept-package-agreements --accept-source-agreements
 
 rem # --- Git For Windows --- #
-winget install git.git --accept-package-agreements --accept--source-agreements
+winget install git.git --accept-package-agreements --accept-source-agreements
 
 rem # --- VSCodium --- #
-winget install vscodium.vscodium --accept-package-agreements --accept--source-agreements
+winget install vscodium.vscodium --accept-package-agreements --accept-source-agreements
 
 rem # --- Brave --- #
-winget install brave.brave --accept-package-agreements --accept--source-agreements
+winget install brave.brave --accept-package-agreements --accept-source-agreements
 
 rem # --- OBS Studio --- #
-winget install obsproject.obsstudio --accept-package-agreements --accept--source-agreements
+winget install obsproject.obsstudio --accept-package-agreements --accept-source-agreements
 
 rem # --- Steam --- #
-winget install valve.steam --accept-package-agreements --accept--source-agreements
+winget install valve.steam --accept-package-agreements --accept-source-agreements
 
 rem # --- Prism Launcher [Minecraft] --- #
-winget install prismlauncher.prismlauncher --accept-package-agreements --accept--source-agreements
+winget install prismlauncher.prismlauncher --accept-package-agreements --accept-source-agreements
