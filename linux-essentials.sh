fc6efcf7eaa1fd506a0a239539aab49bc8734598
ClaytonTDM <claytontdm@gmail.com>
2023-04-17 03:05:51 -0400

    Update and rename mint-essentials.sh to linux-essentials.sh

--- linux-essentials.sh
+++ linux-essentials.sh
@@ -53,40 +53,41 @@ sudo apt update > /dev/null;
 sudo apt install ffmpeg obs-studio -y > /dev/null;
 
 # =================================================================
-echo "66.67"
-echo "# Installing VSCodium..."
-wget https://github.com/VSCodium/vscodium/releases/download/1.77.2.23101/codium_1.77.2.23101_amd64.deb > /dev/null;
-sudo apt-get install ./codium_1.77.2.23101_amd64.deb -y > /dev/null;
-
-# =================================================================
-echo "75"
+echo "66.7"
 echo "# Installing Discord..."
 wget https://dl.discordapp.net/apps/linux/0.0.26/discord-0.0.26.deb > /dev/null;
 sudo apt-get install ./discord-0.0.26.deb -y > /dev/null;
 
 # =================================================================
-echo "83.33"
+echo "75"
 echo "# Installing Steam..."
 sudo apt-get install steam -y > /dev/null;
 
 # =================================================================
+echo "83.33"
+echo "# Installing VSCodium..."
+curl -s https://api.github.com/repos/VSCodium/vscodium/releases/latest -o codium.deb | jq '.assets[] | select(.name|match("amd64.deb$")) | .browser_download_url'
+sudo apt-get install ./codium.deb -y > /dev/null;
 echo "91.67"
+
+# =================================================================
+echo "93.67"
 echo "# Cleaning Up..."
 sudo apt-get install -f > /dev/null;
 sudo apt-get autoremove -y > /dev/null;
 sudo rm -rf ./discord-0.0.26.deb > /dev/null;
+sudo rm -rf ./codium.deb > /dev/null;
 
 # =================================================================
+
 echo "100"
-
-
 ) |
 zenity --progress \
-  --title="Mint Essentials" \
+  --title="Linux Essentials" \
   --text="Preparing..." \
   --percentage=0 \
   --auto-close \
   --auto-kill
 
 (( $? != 0 )) && zenity --error --text="An error has occurred."
-zenity --info --text="All programs supported by your system have been installed. Thank you for using Mint Essentials." --title="Mint Essentials"
+zenity --info --text="All programs supported by your system have been installed. Thank you for using Linux Essentials." --title="Mint Essentials"
