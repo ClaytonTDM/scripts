0eddec6c2dda9c383f0e94e0e842ef0e5e867b63
ClaytonTDM <claytontdm@gmail.com>
2023-04-22 23:34:56 -0400

    Update ubuntu-essentials.sh

--- ubuntu-essentials.sh
+++ ubuntu-essentials.sh
@@ -47,6 +47,8 @@ sudo apt-get install git -y > /dev/null;
 # =================================================================
 echo "50"
 echo "# Installing Spotify..."
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update > /dev/null;
 sudo apt-get install spotify-client -y > /dev/null;
 
