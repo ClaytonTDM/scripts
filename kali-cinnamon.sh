736962452ae8662533dfa84440a9746473bbadf5
ClaytonTDM <claytontdm@gmail.com>
2023-05-13 12:18:59 -0400

    Create kali-cinnamon.sh

---
+++ kali-cinnamon.sh
@@ -0,0 +1,8 @@
+sudo rm -rf /etc/apt/sources.list.d/kalicinnamon.list
+sudo bash -c "echo deb [trusted=yes] http://packages.linuxmint.com cindy main > /etc/apt/sources.list.d/kalicinnamon.list"
+sudo apt update
+sudo apt install kali-defaults kali-root-login desktop-base cinnamon mint-x-icons mint-y-icons mint-themes bibata-cursor-theme -y
+zenity --info \
+--text="To switch to Cinnamon, select \"/usr/bin/cinnamon-session\" (usually selection 1) in the next step."
+clear
+sudo update-alternatives --config x-session-manager
