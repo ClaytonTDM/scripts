839093e6886f82e1b1a36612355991f2715f9d15
ClaytonTDM <claytontdm@gmail.com>
2023-05-15 20:32:35 -0400

    Add cls

--- windows-essentials.bat
+++ windows-essentials.bat
@@ -1,22 +1,32 @@
 @echo off
 
+cls
 rem # --- Discord --- #
 winget install discord.discord --accept-package-agreements --accept-source-agreements
 
+cls
 rem # --- Git For Windows --- #
 winget install git.git --accept-package-agreements --accept-source-agreements
 
+cls
 rem # --- VSCodium --- #
 winget install vscodium.vscodium --accept-package-agreements --accept-source-agreements
 
+cls
 rem # --- Brave --- #
 winget install brave.brave --accept-package-agreements --accept-source-agreements
 
+cls
 rem # --- OBS Studio --- #
 winget install obsproject.obsstudio --accept-package-agreements --accept-source-agreements
 
+cls
 rem # --- Steam --- #
 winget install valve.steam --accept-package-agreements --accept-source-agreements
 
+cls
 rem # --- Prism Launcher [Minecraft] --- #
 winget install prismlauncher.prismlauncher --accept-package-agreements --accept-source-agreements
+
+cls
+echo Finished.
