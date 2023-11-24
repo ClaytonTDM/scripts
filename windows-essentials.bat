f6a82e1b1a4c83ce721ca1cbe9c0a6fd0a25da7b
ClaytonTDM <claytontdm@gmail.com>
2023-11-24 17:31:51 -0500

    Update windows-essentials.bat

--- windows-essentials.bat
+++ windows-essentials.bat
@@ -1,32 +1,54 @@
 @echo off
 
-cls
-rem # --- Discord --- #
-winget install discord.discord --accept-package-agreements --accept-source-agreements
+winget install --id=Microsoft.WindowsTerminal -e
+winget install --id=ShareX.ShareX -e
+winget install --id=Notepad++.Notepad++ -e
+winget install --id=Valve.Steam -e
+winget install --id=Discord.Discord -e
+winget install --id=pizzaboxer.Bloxstrap -e
+winget install --id=Eassos.DiskGenius -e
+winget install --id=Microsoft.VisualStudioCode -e
+winget install --id=Microsoft.VCRedist.2015+.x64 -e
+winget install --id=Microsoft.VCRedist.2015+.x86 -e
+winget install --id=Microsoft.VCRedist.2013.x86 -e
+winget install --id=Microsoft.VCRedist.2013.x64 -e
+winget install --id=Microsoft.VCRedist.2012.x86 -e
+winget install --id=Microsoft.VCRedist.2012.x64 -e
+winget install --id=Microsoft.VCRedist.2010.x86 -e
+winget install --id=Microsoft.VCRedist.2010.x64 -e
+winget install --id=Microsoft.VCRedist.2005.x64 -e
+winget install --id=Microsoft.VCRedist.2008.x64 -e
+winget install --id=Microsoft.DotNet.DesktopRuntime.5 -e
+winget install --id=Microsoft.DotNet.HostingBundle.5 -e
+winget install --id=Microsoft.DotNet.AspNetCore.5 -e
+winget install --id=Microsoft.DotNet.Runtime.5 -e
+winget install --id=Microsoft.DotNet.Runtime.6 -e
+winget install --id=Microsoft.DotNet.Runtime.Preview -e
+winget install --id=Microsoft.DotNet.AspNetCore.Preview -e
+winget install --id=Microsoft.DotNet.DesktopRuntime.3_1 -e
+winget install --id=Microsoft.DotNet.HostingBundle.3_1 -e
+winget install --id=Microsoft.DotNet.AspNetCore.6 -e
+winget install --id=Microsoft.DotNet.AspNetCore.3_1 -e
+winget install --id=Microsoft.DotNet.DesktopRuntime.6 -e
+winget install --id=Microsoft.DotNet.Runtime.3_1 -e
+winget install --id=Microsoft.DotNet.DesktopRuntime.Preview -e
+winget install --id=Microsoft.DotNet.Runtime.7 -e
+winget install --id=Microsoft.DotNet.SDK.7 -e
+winget install --id=Microsoft.DotNet.AspNetCore.8 -e
+winget install --id=Microsoft.DotNet.DesktopRuntime.7 -e
+winget install --id=Microsoft.DotNet.AspNetCore.7 -e
+winget install --id=Microsoft.DotNet.Runtime.8 -e
+winget install --id=Microsoft.DotNet.DesktopRuntime.8 -e
+winget install --id=Microsoft.dotnetRuntime.3-x64 -e
+winget install --id=Microsoft.VSTOR -e
+winget install --id=PrismLauncher.PrismLauncher -e
+winget install --id=Mojang.MinecraftLauncher -e
+winget install --id=OBSProject.OBSStudio -e
+winget install --id=Brave.Brave -e
+winget install --id=Microsoft.PowerToys -e
+winget install --id=RARLab.WinRAR -e
+winget install --id=EpicGames.EpicGamesLauncher -e
+winget install --id=OpenJS.NodeJS -e
 
-cls
-rem # --- Git For Windows --- #
-winget install git.git --accept-package-agreements --accept-source-agreements
-
-cls
-rem # --- VSCodium --- #
-winget install vscodium.vscodium --accept-package-agreements --accept-source-agreements
-
-cls
-rem # --- Brave --- #
-winget install brave.brave --accept-package-agreements --accept-source-agreements
-
-cls
-rem # --- OBS Studio --- #
-winget install obsproject.obsstudio --accept-package-agreements --accept-source-agreements
-
-cls
-rem # --- Steam --- #
-winget install valve.steam --accept-package-agreements --accept-source-agreements
-
-cls
-rem # --- Prism Launcher [Minecraft] --- #
-winget install prismlauncher.prismlauncher --accept-package-agreements --accept-source-agreements
-
-cls
+echo.
 echo Finished.
