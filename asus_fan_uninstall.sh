1d50398ad193569004dfbbcc3f1e0416a9116913
Clay <claytontdm@gmail.com>
2025-03-08 01:07:53 -0500

    Create asus_fan_uninstall.sh

---
+++ asus_fan_uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -f /usr/local/bin/asus_fan_notify.py ]; then
+    rm -f /usr/local/bin/asus_fan_notify.py
+    echo "Removed /usr/local/bin/asus_fan_notify.py"
+fi
+
+if [ -f /usr/local/bin/asus_fan_monitor.sh ]; then
+    rm -f /usr/local/bin/asus_fan_monitor.sh
+    echo "Removed /usr/local/bin/asus_fan_monitor.sh"
+fi
+
+if [ -f /home/$(logname)/.config/autostart/asus-fan-monitor.desktop ]; then
+    rm -f /home/$(logname)/.config/autostart/asus-fan-monitor.desktop
+    echo "Removed autostart entry"
+fi
+
+if [ -f /etc/sudoers.d/asus-fan-monitor ]; then
+    rm -f /etc/sudoers.d/asus-fan-monitor
+    echo "Removed sudoers entry"
+fi
+
+if [ -f /tmp/asus_fan_notification_id ]; then
+    rm -f /tmp/asus_fan_notification_id
+    echo "Removed temporary files"
+fi
+
+pkill -f "asus_fan_monitor.sh" 2>/dev/null
+echo "Stopped any running instances"
+
+echo "ASUS Fan Monitor has been completely uninstalled."
