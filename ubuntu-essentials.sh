b5fe21694aeb972796af5f6896932b7f02cc79ed
Clay <claytontdm@gmail.com>
2025-03-08 01:27:01 -0500

    mit license update

--- ubuntu-essentials.sh
+++ ubuntu-essentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-ans=$(zenity --list --text="This tool is licensed under the Eclipse Public License 2.0.\n\nThis tool is provided as-is without any warranty\nor guarantee of any kind, whether express or implied.\nThe user assumes all risks associated with the use of this tool.\n\nThe developer of this tool shall not be liable for any damages\nor losses of any kind arising from the use or inability to use this\ntool, including but not limited to direct, indirect, incidental,\npunitive, and consequential damages." --radiolist --column="" --column="" --title="Ubuntu Essentials - Agreement" TRUE Agree FALSE Disagree);
+ans=$(zenity --list --text="This tool is licensed under the MIT License.\n\nThis tool is provided as-is without any warranty\nor guarantee of any kind, whether express or implied.\nThe user assumes all risks associated with the use of this tool.\n\nThe developer of this tool shall not be liable for any damages\nor losses of any kind arising from the use or inability to use this\ntool, including but not limited to direct, indirect, incidental,\npunitive, and consequential damages." --radiolist --column="" --column="" --title="Ubuntu Essentials - Agreement" TRUE Agree FALSE Disagree);
 if [ "$ans" == "Agree" ]; then
 function askPassword {
   if [ $(sudo -n uptime 2>&1 | grep "load" | wc -l) != "1" ]; then
