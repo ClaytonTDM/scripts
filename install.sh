a93894e7ca251fb433a595bef3b94d7e979edc6b
Clay <claytontdm@gmail.com>
2025-04-25 05:19:17 -0400

    sdadsadasdsadadasd

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ read -n 1 -s key
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# cli stuff
-sudo apt-get install git gh htop zsh -y
+# initial stuff
+sudo apt-get install -y -m git zsh
 
 # oh my posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
@@ -150,7 +150,7 @@ curl -fsSL https://bun.sh/install | bash
 # now graphical stuff
 
 # deb packages
-sudo apt-get install -y -m diodon audacity wine winetricks protontricks lutris steam retroarch flameshot gimp gnome-clocks dconf-editor ibus obs-studio vlc vlc-plugin-fluidsynth libonig5 libsass1 inkscape optipng zoxide power-profiles-daemon
+sudo apt-get install -y -m diodon audacity wine winetricks protontricks lutris steam retroarch flameshot gimp gnome-clocks dconf-editor ibus obs-studio vlc vlc-plugin-fluidsynth libonig5 libsass1 inkscape optipng zoxide power-profiles-daemon gh htop yad
 # flatpaks
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub com.usebottles.bottles com.github.tchx84.Flatseal org.prismlauncher.PrismLauncher com.vysp3r.ProtonPlus org.vinegarhq.Vinegar so.libdb.dissent com.github.neithern.g4music io.github.cboxdoerfer.FSearch org.vinegarhq.Sober
@@ -182,9 +182,9 @@ GHOSTTY_DEB_FILE=$(basename "$GHOSTTY_DEB_URL")
 curl -LO "$GHOSTTY_DEB_URL"
 sudo dpkg -i "$GHOSTTY_DEB_FILE"
 rm "$GHOSTTY_DEB_FILE"
-sudo apt install --fix-broken -y
+sudo apt-get install --fix-broken -y
 # vesktop
-curl -L -o vesktop.deb "https://github.com/Vencord/Vesktop/releases/download/v1.5.4/vesktop_1.5.4_amd64.deb"
+curl -L -o vesktop.deb "https://github.com/Vencord/Vesktop/releases/download/v1.5.6/vesktop_1.5.6_amd64.deb"
 sudo apt-get install ./vesktop.deb -y
 rm vesktop.deb
 # zen browser
@@ -206,12 +206,13 @@ rm -rf cascadia cascadia.zip
 fc-cache -fv
 
 # install colloid gtk theme
-mkdir ~/Desktop/DEV # dont delete this folder
+mkdir ~/Desktop/DEV
 cd ~/Desktop/DEV
+echo "It's recommended to use this folder for Git projects" > README
 git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
 cd Colloid-gtk-theme
 sudo chmod +x install.sh
-./install.sh -t pink -c dark --tweaks black rimless -l
+./install.sh -t pink -c dark --tweaks black -l
 
 # generate icons
 cd ~/Desktop/DEV
@@ -229,46 +230,44 @@ sudo cp -rf ./share/* /usr/share/
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# set default apps
 xdg-settings set default-web-browser app.zen_browser.zen.desktop
 gsettings set org.cinnamon.desktop.default-applications.terminal exec ghostty
 sudo update-alternatives --set x-terminal-emulator /usr/bin/ghostty
-# set default fonts
+
 gsettings set org.cinnamon.desktop.interface font-name 'Inter 10'
 gsettings set org.nemo.desktop font 'Inter 10'
 gsettings set org.gnome.desktop.interface document-font-name 'Inter 10'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Cascadia Mono 10'
 gsettings set org.cinnamon.desktop.wm.preferences titlebar-font 'Inter Medium 10'
-# set gtk theme
+
 gsettings set org.cinnamon.desktop.interface gtk-theme 'Colloid-Pink-Dark'
-# set icon theme
 gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Colloid-Pink'
-# set mouse pointer
 gsettings set org.cinnamon.desktop.interface cursor-theme 'Bibata-Modern-Classic'
-# set cinnamon theme
 gsettings set org.cinnamon.theme name 'Colloid-Pink-Dark'
 
 # restart cinnamon
 cinnamon -r >/dev/null 2>&1 &
 
 # show popup
-zenity --list \
+yad --list \
 --title="Installation Complete" \
 --width=400 \
 --height=300 \
---text="You may notice some issues like the terminal font being\nwonky - these issues can be fixed by rebooting your system.\n\nThe following apps were installed:" \
+--text="\n     You may notice some issues like the terminal font being\n     wonky - these issues will be fixed on the next startup.\n\n     <b>Pressing Close will immediately reboot your system.</b>\n\n     The following packages were installed:\n" \
 --column="Installed Applications" \
---ok-label="Close" \
+--button="Close:0" \
 --hide-header \
 "Git" \
 "GitHub CLI" \
-"HTOP" \
-"Z Shell" \
+"htop" \
+"zsh" \
+"zsh plugin - zsh syntax highlighting" \
+"zsh plugin - Autosuggestions" \
 "Oh My Posh" \
 "Antidote" \
 "Node Version Manager" \
 "Node.js" \
-"PNPM" \
+"pnpm" \
 "Deno" \
 "Bun" \
 "Diodon" \
@@ -280,19 +279,18 @@ zenity --list \
 "Steam" \
 "RetroArch" \
 "Flameshot" \
-"GNU Image Manipulation Program" \
-"GNOME Camera" \
+"GNU Image Manipulation Program (GIMP)" \
 "GNOME Clocks" \
 "DConf Editor" \
-"Intelligent Input Bus" \
-"Open Broadcaster Software Studio" \
-"VideoLAN Client" \
-"VideoLAN Client FluidSynth Plugin" \
+"IBus" \
+"OBS Studio" \
+"VLC media player" \
+"VLC FluidSynth Plugin" \
 "Sober" \
 "Bottles" \
 "Flatseal" \
 "FSearch" \
-"PrismLauncher" \
+"Prism Launcher" \
 "ProtonPlus" \
 "Vinegar" \
 "Zen Browser" \
@@ -301,10 +299,15 @@ zenity --list \
 "Brave Browser" \
 "Ghostty" \
 "Vesktop" \
+"Dissent" \
 "Zoxide" \
 "Inkscape" \
-"Inter Font" \
-"Cascadia Code Font" \
-"Colloid Pink Dark GTK Theme" \
-"Colloid Pink Dark Cinnamon Theme" \
-"Colloid Pink Mint Y Icon Theme"
+"OptiPNG" \
+"YAD" \
+"Font - Inter" \
+"Font - Cascadia Code" \
+"GTK Theme - Colloid Pink Dark" \
+"Cinnamon Theme - Colloid Pink Dark" \
+"Icon Theme - Mint Y Colloid Pink"
+
+sudo reboot
