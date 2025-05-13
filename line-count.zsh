6ecb005aa81848720f8c48095194ac8fe0d87e6c
Clay <claytontdm@gmail.com>
2025-05-13 00:00:46 -0400

    Update line-count.zsh

--- line-count.zsh
+++ line-count.zsh
@@ -1,5 +1,5 @@
-printf "<files>" && find . -path './.git' -prune -o -type f -exec file --mime {} + | \
-  grep 'text/' | cut -d: -f1 | while read -r file; do
+printf "<files>" && find . -path './.git' -prune -o -path './node_modules' -prune -o -type f -exec file --mime {} + | \
+  grep 'text/' | cut -d: -f1 | while IFS= read -r file; do
   lines=$(wc -l < "$file")
   safe_file=$(echo "$file" | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g')
   printf "<file name=\"%s\" lines=\"%s\" />" "$safe_file" "$lines"
