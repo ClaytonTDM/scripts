4efd6ac2ac01e2f4ab59ca276ad69db54b56a707
Clay <claytontdm@gmail.com>
2025-05-12 23:58:14 -0400

    Create line-count.zsh

---
+++ line-count.zsh
@@ -0,0 +1,6 @@
+printf "<files>" && find . -path './.git' -prune -o -type f -exec file --mime {} + | \
+  grep 'text/' | cut -d: -f1 | while read -r file; do
+  lines=$(wc -l < "$file")
+  safe_file=$(echo "$file" | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g')
+  printf "<file name=\"%s\" lines=\"%s\" />" "$safe_file" "$lines"
+done && printf "</files>"
